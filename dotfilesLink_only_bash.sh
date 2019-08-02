bc1e55de7da9450a890c050f72a66434b8b9c1fa
tamata78 <5101956+tamata78@users.noreply.github.com>
2019-08-02 21:14:12 +0900

    add exclude dotfiles

--- dotfilesLink_only_bash.sh
+++ dotfilesLink_only_bash.sh
@@ -6,6 +6,11 @@ do
     [[ "$f" == ".DS_Store" ]] && continue
     [[ "$f" == ".zprofile" ]] && continue
     [[ "$f" == ".zshrc" ]] && continue
+    [[ "$f" == ".enhancd" ]] && continue
+    [[ "$f" == ".ssh" ]] && continue
+    [[ "$f" == ".viminfo" ]] && continue
+    [[ "$f" == ".zshrc" ]] && continue
+    [[ "$f" == ".zshrc" ]] && continue
 
     echo "$f"
     ln -sf ~/dotfiles/$f ~/$f
